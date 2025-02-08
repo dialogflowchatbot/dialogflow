d1d6a3de895ee8ee3a22e7283d03db828ddcd1ec
dialogflowchatbot <142310293+dialogflowchatbot@users.noreply.github.com>
2025-02-08 14:18:29 +0800

    Update RequestHandlerTest.java

--- sdk/java/src/test/java/io/github/dialogflowchatbot/RequestHandlerTest.java
+++ sdk/java/src/test/java/io/github/dialogflowchatbot/RequestHandlerTest.java
@@ -13,8 +13,8 @@ public class RequestHandlerTest {
     @Test
     void reqTest() throws IOException, InterruptedException {
         RequestData requestData = RequestData.create("r03dbzxp6zpk9uhkgcbw1ec604", "103dbzxp74kjwb148ubfmhgemb");
-        RequestHandler requestHandler = new RequestHandler();
-        Response res = requestHandler.req("http://10.247.144.182:12715/flow/answer", requestData, 1000);
+        RequestHandler requestHandler = new RequestHandler("http://10.247.144.182:12715/flow/answer");
+        Response res = requestHandler.req(requestData, 1000);
         assertNotNull(res);
         assertNotNull(res.getData());
         assertEquals(1, res.getData().getAnswers().size());
