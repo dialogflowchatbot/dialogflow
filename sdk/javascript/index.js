60044c02518e0187f135ba19a6429ca4b712cbad
dialogflowchatbot <dialogflow@yeah.net>
2025-02-09 11:16:25 +0800

    Some minor fixes

--- sdk/javascript/index.js
+++ sdk/javascript/index.js
@@ -4,34 +4,42 @@ class DialogFlowChatBotSDK {
         this.url = url;
     }
 
-    const VarKind = Object.freeze({
+    VarKind = Object.freeze({
         STRING: 'String',
         NUMBER: 'Number',
     });
 
-    const UserInputResult = Object.freeze({
-    SUCCESSFUL:'Successful',
-    TIMEOUT:'Timeout',
+    UserInputResult = Object.freeze({
+        SUCCESSFUL: 'Successful',
+        TIMEOUT: 'Timeout',
     });
 
     genRequestData(robotId, mainFlowId) {
-    return {
-             robotId: robotId,
-             mainFlowId: mainFlowId,
-             sessionId: "",
-             userInputResult: this.UserInputResult.SUCCESSFUL,
-             userInput: "",
-             importVariables: [],
-             userInputIntent: ""
-           };
+        return {
+            robotId: robotId,
+            mainFlowId: mainFlowId,
+            sessionId: "",
+            userInputResult: this.UserInputResult.SUCCESSFUL,
+            userInput: "",
+            importVariables: [],
+            userInputIntent: ""
+        };
     }
 
     async postData(data) {
+        if (data.sessionId == null)
+            data.sessionId = '';
+        if (data.userInput == null)
+            data.userInput = '';
+        if (data.userInputResult == null)
+            data.userInputResult = this.UserInputResult.SUCCESSFUL;
+        if (data.importVariables == null)
+            data.importVariables = [];
         const response = await fetch(this.url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-//                'Authorization': `Bearer ${this.apiKey}`
+                //                'Authorization': `Bearer ${this.apiKey}`
             },
             body: JSON.stringify(data)
         });
